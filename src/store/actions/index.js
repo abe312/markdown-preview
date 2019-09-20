073776d5dfecf179f77c83899b0fbb914c040916
abe312 <abhineet312@gmail.com>
2019-09-21 03:54:46 +0530

    some cleaning up

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -6,33 +6,3 @@ const updateInput = event => ({
 });
 
 export { updateInput };
-
-// const loadImages = () => ({
-//     type: IMAGES.LOAD,
-// });
-
-// const setImages = images => ({
-//     type: IMAGES.LOAD_SUCCESS,
-//     images,
-// });
-
-// const setError = error => ({
-//     type: IMAGES.LOAD_FAIL,
-//     error,
-// });
-
-// const loadImagesStats = id => ({
-//     type: STATS.LOAD,
-//     id,
-// });
-
-// const setImagesStats = (id, downloads) => ({
-//     type: STATS.LOAD_SUCCESS,
-//     id,
-//     downloads,
-// });
-
-// const setStatsError = id => ({
-//     type: STATS.LOAD_FAIL,
-//     id,
-// });
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,13 +1,9 @@
 import updateInputReducer from './updateInputReducer';
-// import loadingReducer from './loadingReducer';
-// import errorReducer from './errorReducer';
 
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   input: updateInputReducer,
-  // isLoading: loadingReducer,
-  // error: errorReducer,
 });
 
 export default rootReducer;
